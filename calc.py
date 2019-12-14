76b0b42aa9e27ba4d24394f93b1905f3d9c5bada
Ghost91000 <vald.velichko.90@gmail.com>
2019-12-14 16:15:53 +0200

    PotashBrackets

--- calc.py
+++ calc.py
@@ -1,110 +1,105 @@
-base = "3+5+6+(5+6)"#i nput("Введите выражение для вычисления: ")
+base = input("Введите выражение для вычисления: ")
 base = base.replace(' ', '')
+outp = base
+
 # base перемннная ввода
-operands = []
-# массив для хранения операндов(чисел)
-operators = []
-# массив для хранения операторов
-begin = 0
-end = 1
 
 
-
-# блок обработки строки в массивы, принимает строку и возвращает масссив чисел и знаков
 # ДОРАБОТАТЬ ВОЗМОЖНОСТЬ ВВОДА ПЕРВОГО ЗНАКА, НАПРИМЕР -1
-while True:
-    print(f"begin = {begin}, end = {end}")
-    if end >= len(base):
-        operands.append(int(base[begin:end]))
-        # выполняется в конце обработки строки, добовляя последнее число
-        break
-    try:
-        """ 
-        Этот блок должен ловить ошибки ввода, в случае если введены другие символы кроме чисел и знаков
-        Следует доработать функционал поиска ошибок, т.к. Ошибка выводится, но код продолжает выполняться в прежнем режиме
-        """
-        int(base[begin:end])
-    except ValueError:
-        print("Вероятно число введено неверно")
-
-    if '+' in base[begin:end+1]:
-        # ЭТОТ БЛОК ИМЕЕТ СМЫСЛ ДОБАВИТЬ В ФУНКЦИЮ!
-        operators.append(base[end])
-        # добовление первого оператора в массив
-        operands.append(int(base[begin:end]))
-        # доблвление первого операнда
-        # assert(operands[0] == 20), f"первый оператор определен неправильно, ожидалось 2, получено {operands[0]}"
-        # проверка первого оператора
-        begin = end+1
-        end = end+2
-        continue
-
-    elif '-' in base[begin:end+1]:
-        # ЭТОТ БЛОК ИМЕЕТ СМЫСЛ ДОБАВИТЬ В ФУНКЦИЮ!
-        operators.append(base[end])
-        # добовление первого оператора в массив
-        operands.append(int(base[begin:end]))
-        # доблвление первого операнда
-        # assert(operands[0] == 20), f"первый оператор определен неправильно, ожидалось 2, получено {operands[0]}"
-        # проверка первого оператора
-        begin = end+1
-        end = end+2
-        continue
-    elif '/' in base[begin:end+1]:
-        # ЭТОТ БЛОК ИМЕЕТ СМЫСЛ ДОБАВИТЬ В ФУНКЦИЮ!
-        operators.append(base[end])
-        # добовление первого оператора в массив
-        operands.append(int(base[begin:end]))
-        # доблвление первого операнда
-        # assert(operands[0] == 20), f"первый оператор определен неправильно, ожидалось 2, получено {operands[0]}"
-        # проверка первого оператора
-        begin = end+1
-        end = end+2
-        continue
-    elif '*' in base[begin:end+1]:
-        # ЭТОТ БЛОК ИМЕЕТ СМЫСЛ ДОБАВИТЬ В ФУНКЦИЮ!
-        operators.append(base[end])
-        # добовление первого оператора в массив
-        operands.append(int(base[begin:end]))
-        # доблвление первого операнда
-        # assert(operands[0] == 20), f"первый оператор определен неправильно, ожидалось 2, получено {operands[0]}"
-        # проверка первого оператора
-        begin = end+1
-        end = end+2
-        continue
-    elif '(' in base[begin:end+1]:
-        # ЭТОТ БЛОК ИМЕЕТ СМЫСЛ ДОБАВИТЬ В ФУНКЦИЮ!
-        operators.append(base[end])
-        # добовление первого оператора в массив
-        operands.append(int(base[begin:end]))
-        # доблвление первого операнда
-        # assert(operands[0] == 20), f"первый оператор определен неправильно, ожидалось 2, получено {operands[0]}"
-        # проверка первого оператора
-        begin = end+1
-        end = end+2
-        continue
-    elif ')' in base[begin:end+1]:
-        # ЭТОТ БЛОК ИМЕЕТ СМЫСЛ ДОБАВИТЬ В ФУНКЦИЮ!
-        operators.append(base[end])
-        # добовление первого оператора в массив
-        operands.append(int(base[begin:end]))
-        # доблвление первого операнда
-        # assert(operands[0] == 20), f"первый оператор определен неправильно, ожидалось 2, получено {operands[0]}"
-        # проверка первого оператора
-        begin = end+1
-        end = end+2
-        continue
-    end = end + 1
 
 
-print(operators)
-print(operands)
+def str_to_source(inp_string):
+    # Функция принимает строку и распределяет ее на массив чисел и массив знаков
+    begin = 0
+    end = 1
+    operands = []
+    # массив для хранения операндов(чисел)
+    operators = []
+    # массив для хранения операторов
+    while True:
+        # print(f"begin = {begin}, end = {end}")
+        if end >= len(inp_string):
+            operands.append(int(inp_string[begin:end]))
+            # выполняется в конце обработки строки, добовляя последнее число
+            break
+        try:
+            """ 
+            Этот блок должен ловить ошибки ввода, в случае если введены другие символы кроме чисел и знаков
+            Следует доработать функционал поиска ошибок, т.к. Ошибка выводится, но код продолжает выполняться в прежнем режиме
+            """
+            int(inp_string[begin:end])
+        except ValueError:
+            print("Вероятно число введено неверно")
 
+        if '+' in inp_string[begin:end + 1]:
+            # ЭТОТ БЛОК ИМЕЕТ СМЫСЛ ДОБАВИТЬ В ФУНКЦИЮ!
+            operators.append(inp_string[end])
+            # добовление первого оператора в массив
+            operands.append(int(inp_string[begin:end]))
+            # доблвление первого операнда
+            # assert(operands[0] == 20), f"первый оператор определен неправильно, ожидалось 2, получено {operands[0]}"
+            # проверка первого оператора
+            begin = end + 1
+            end = end + 2
+            continue
 
+        elif '-' in inp_string[begin:end + 1]:
+            # ЭТОТ БЛОК ИМЕЕТ СМЫСЛ ДОБАВИТЬ В ФУНКЦИЮ!
+            operators.append(inp_string[end])
+            # добовление первого оператора в массив
+            operands.append(int(inp_string[begin:end]))
+            # доблвление первого операнда
+            # assert(operands[0] == 20), f"первый оператор определен неправильно, ожидалось 2, получено {operands[0]}"
+            # проверка первого оператора
+            begin = end + 1
+            end = end + 2
+            continue
+        elif '/' in inp_string[begin:end + 1]:
+            # ЭТОТ БЛОК ИМЕЕТ СМЫСЛ ДОБАВИТЬ В ФУНКЦИЮ!
+            operators.append(inp_string[end])
+            # добовление первого оператора в массив
+            operands.append(int(inp_string[begin:end]))
+            # доблвление первого операнда
+            # assert(operands[0] == 20), f"первый оператор определен неправильно, ожидалось 2, получено {operands[0]}"
+            # проверка первого оператора
+            begin = end + 1
+            end = end + 2
+            continue
+        elif '*' in inp_string[begin:end + 1]:
+            # ЭТОТ БЛОК ИМЕЕТ СМЫСЛ ДОБАВИТЬ В ФУНКЦИЮ!
+            operators.append(inp_string[end])
+            # добовление первого оператора в массив
+            operands.append(int(inp_string[begin:end]))
+            # доблвление первого операнда
+            # assert(operands[0] == 20), f"первый оператор определен неправильно, ожидалось 2, получено {operands[0]}"
+            # проверка первого оператора
+            begin = end + 1
+            end = end + 2
+            continue
+        elif '(' in inp_string[begin:end + 1]:
+            # ЭТОТ БЛОК ИМЕЕТ СМЫСЛ ДОБАВИТЬ В ФУНКЦИЮ!
+            operators.append(inp_string[end])
+            # добовление первого оператора в массив
+            operands.append(int(inp_string[begin:end]))
+            # доблвление первого операнда
+            # assert(operands[0] == 20), f"первый оператор определен неправильно, ожидалось 2, получено {operands[0]}"
+            # проверка первого оператора
+            begin = end + 1
+            end = end + 2
+            continue
+        elif ')' in inp_string[begin:end + 1]:
+            # ЭТОТ БЛОК ИМЕЕТ СМЫСЛ ДОБАВИТЬ В ФУНКЦИЮ!
+            operators.append(inp_string[end])
+            # добовление первого оператора в массив
+            operands.append(int(inp_string[begin:end]))
+            # доблвление первого операнда
+            # assert(operands[0] == 20), f"первый оператор определен неправильно, ожидалось 2, получено {operands[0]}"
+            # проверка первого оператора
+            begin = end + 1
+            end = end + 2
+            continue
+        end = end + 1
 
-"""
-# функция выполнения математических операций
-def calculating(operators, operands):
 
     for sign in operators:
         if sign == '*':
@@ -113,11 +108,11 @@ def calculating(operators, operands):
         if sign == '/':
             operands[operators.index('/')] /= operands[operators.index('/') + 1]
             del operands[operators.index('/') + 1]
-    
+
     i = 1
     # индекс операндов
     ans = operands[0]
-    
+
     # выполняет операции сложения, вычитания.
     for sign in operators:
         if sign == '+':
@@ -129,6 +124,29 @@ def calculating(operators, operands):
     return ans
 
 
-ans = calculating(operators, operands)
-print(f"{base} = {ans}")
-"""
+mass_op = []
+mass_cl = []
+i = 0
+for a in base:
+    if a == "(":
+        mass_op.append(i)
+    elif a == ")":
+        mass_cl.append(i)
+    i += 1
+assert (len(mass_cl) == len(mass_op)), "Ошибка ввода! Введены неверные скобки"
+
+op = -1
+cl = 0
+while True:
+    # print(base[base.rfind('(') + 1:base.find(')')])
+    base = base[0:base.rfind('(')] + str(str_to_source(base[base.rfind('(') + 1:base.find(')')])) + base[base.find(')') + 1:len(base)]
+    # print(base)
+
+    cl += 1
+    if cl >= len(mass_cl):
+        break
+
+print(f"{outp} = {base}")
+
+# ans = str_to_source(base)
+# print(f"{base} = {ans}")
