5f7f6f8973b09af7c971b95ea650d69b2fd8d605
Ghost91000 <vald.velichko.90@gmail.com>
2019-12-18 14:33:28 +0200

    some changes

--- calc.py
+++ calc.py
@@ -76,35 +76,13 @@ def str_to_source(inp_string):
             begin = end + 1
             end = end + 2
             continue
-        elif '(' in inp_string[begin:end + 1]:
-            # ЭТОТ БЛОК ИМЕЕТ СМЫСЛ ДОБАВИТЬ В ФУНКЦИЮ!
-            operators.append(inp_string[end])
-            # добовление первого оператора в массив
-            operands.append(int(inp_string[begin:end]))
-            # доблвление первого операнда
-            # assert(operands[0] == 20), f"первый оператор определен неправильно, ожидалось 2, получено {operands[0]}"
-            # проверка первого оператора
-            begin = end + 1
-            end = end + 2
-            continue
-        elif ')' in inp_string[begin:end + 1]:
-            # ЭТОТ БЛОК ИМЕЕТ СМЫСЛ ДОБАВИТЬ В ФУНКЦИЮ!
-            operators.append(inp_string[end])
-            # добовление первого оператора в массив
-            operands.append(int(inp_string[begin:end]))
-            # доблвление первого операнда
-            # assert(operands[0] == 20), f"первый оператор определен неправильно, ожидалось 2, получено {operands[0]}"
-            # проверка первого оператора
-            begin = end + 1
-            end = end + 2
-            continue
-        end = end + 1
 
+        end = end + 1
 
     for sign in operators:
         if sign == '*':
-            operands[operators.index('*')] *= operands[operators.index('*')+1]
-            del operands[operators.index('*')+1]
+            operands[operators.index('*')] *= operands[operators.index('*') + 1]
+            del operands[operators.index('*') + 1]
         if sign == '/':
             operands[operators.index('/')] /= operands[operators.index('/') + 1]
             del operands[operators.index('/') + 1]
@@ -139,8 +117,11 @@ if '(' in base:
     op = -1
     cl = 0
     while True:
-        print(base[base.rfind('(') + 1:base.find(')')])
-        base = base[0:base.rfind('(')] + str(str_to_source(base[base.rfind('(') + 1:base.find(')', base.rfind('('))])) + base[base.find(')') + 1:len(base)]
+        ind_begin = base.rfind('(')
+        ind_end = base.find(')', ind_begin)
+
+        print(base[ind_begin + 1:ind_end])
+        base = base[0:ind_begin] + str(str_to_source(base[ind_begin + 1:ind_end])) + base[ind_end + 1:len(base)]
         print(base)
 
         cl += 1
